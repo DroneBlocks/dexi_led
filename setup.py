ae50d8e2cd6892b3cbfffbe146053b7506d7bfe3
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-04-05 13:43:43 -0500

    Add launch file

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'dexi_led'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), glob('dexi_led/launch/*.launch.py')),
     ],
     install_requires=['setuptools', 'pi5neo'],
     zip_safe=True,
